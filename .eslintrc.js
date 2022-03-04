e9a3d8eb29bcee085c66a3735f6a211e95355c63
Matt Williams <email.m@ttwill.uk>
2022-03-04 11:02:23 +0000

    init

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: ['xo'],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['@typescript-eslint', 'jest'],
+	rules: {
+	},
+};
